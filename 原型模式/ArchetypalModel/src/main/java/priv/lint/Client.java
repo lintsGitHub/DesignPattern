a0a7036e247a4b6c9e8cb33fd56f9d30ee7e5104
LLS <2145604059@qq.com>
2018-12-10 20:33:39 +0800

    2018/12/10

---
+++ 原型模式/ArchetypalModel/src/main/java/priv/lint/Client.java
@@ -0,0 +1,37 @@
+package priv.lint;
+
+class Prototype implements Cloneable {
+    public Prototype getPrototype() {
+        Prototype prototype = null;
+        try {
+            prototype = (Prototype) super.clone();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+        return prototype;
+    }
+}
+
+class SayHello extends Prototype {
+    public void sayHello() {
+        System.out.println("hello");
+    }
+}
+
+/*
+* 原型模式(引自别人的笔记)：
+* 使用原型模式创建对象比直接new一个对象在性能上要好的多，因为Object类的clone方法是一个本地方法，它直接操作内存中的二进制流，特别是复制大对象时，性能的差别非常明显。
+* 使用原型模式的另一个好处是简化对象的创建，使得创建对象就像我们在编辑文档时的复制粘贴一样简单。
+* 因为以上优点，所以在需要重复地创建相似对象时可以考虑使用原型模式。比如需要在一个循环体内创建对象，假如对象创建过程比较复杂或者循环次数很多的话，
+* 使用原型模式不但可以简化创建过程，而且可以使系统的整体性能提高很多。
+* */
+public class Client {
+    public static void main(String[] args) {
+        SayHello sayHello = new SayHello();
+        for (int i = 0; i < 10; i++) {
+            SayHello prototype = (SayHello) sayHello.getPrototype();
+            System.out.println(prototype);
+            prototype.sayHello();
+        }
+    }
+}
